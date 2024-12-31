6522cef5c0f02612f6d9946132c5328e8419370a
Markus Renken <z0n@users.noreply.github.com>
2024-12-31 21:39:23 +0100

    fix: ask for password when trying to login

--- container-scripts/login.sh
+++ container-scripts/login.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-steamcmd +login $STEAM_USER +quit
+steamcmd +set_var @NoPromptForPassword 0 +set_var @ShutdownOnFailedCommand 0 +login $STEAM_USER +quit
 
 if [ $? -ne 0 ]; then
   echo "Failed to login to Steam. Exiting..."
